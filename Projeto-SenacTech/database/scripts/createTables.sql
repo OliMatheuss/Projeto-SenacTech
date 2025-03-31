99817be0a05183d086123153dc4fafaa94e8400f
GiovaneLM <giovanelimeiramartins@gmail.com>
2025-03-31 10:21:18 -0300

    banco de dados

--- Projeto-SenacTech/database/scripts/createTables.sql
+++ Projeto-SenacTech/database/scripts/createTables.sql
@@ -5,14 +5,13 @@ CREATE TABLE usuarios (
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    pontos INT DEFAULT 1
+    pontos INT DEFAULT 100
 );
 
 CREATE TABLE missoes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario_id INT NOT NULL,
     descricao TEXT NOT NULL,
-    valor_da_missao INT DEFAULT 100,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
